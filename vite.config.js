8bf310ba61f8b7fac20bc8b4ed41d8e8a5554254
iExpertApps <iexpertapps1@gmail.com>
2025-08-17 13:08:19 +0500

    fix: update Vite base path to './' for proper production deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-   base: '/zia-portfolio-react-vite/',
+  base: './', // 👈 relative path, production root ke liye
   plugins: [react()],
 });
